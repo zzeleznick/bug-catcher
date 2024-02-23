a1425ed57e3eb06d1328d4857dec90e5b6dc6aa6
Zach Zeleznick <zz@semgrep.com>
2024-02-23 10:59:51 -0800

    add images for remoteurl

--- src/resources.ts
+++ src/resources.ts
@@ -3,10 +3,15 @@ import {
 }
 from 'https://esm.sh/excalibur@0.26.0-alpha.264';
 
-const server = 'http://localhost:8000';
+// Check if we are running locally vs on a website via the current window location
+// This is a hack to get around CORS issues when loading images
+const _protocol = window?.location?.protocol; // e.g. "http:", "https:", "file:"
+const protocol = _protocol === 'file:' ? 'http:' : _protocol; // use http: if we are running locally
+const host = window?.location?.host ?? 'localhost:8000';
+const serverUrl = `${protocol}//${host}`;
 
-export const semgreppio = new ImageSource(`${server}/img/semgreppio.png`);
-export const codebug = new ImageSource(`${server}/img/code-bug.png`);
+export const semgreppio = new ImageSource(`${serverUrl}/img/semgreppio.png`);
+export const codebug = new ImageSource(`${serverUrl}/img/code-bug.png`);
 
 const Resources = {
     semgreppio,
